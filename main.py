4d405f5160217752b98bd78733c6fd6ff9dbb61d
mab <mab@mab.com>
2023-06-20 11:13:42 +0500

    Slider Working

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
 from flask import Flask, jsonify, send_from_directory
+from flask_cors import CORS
 import os
-from flask_cors import CORS, cross_origin
 
 app = Flask(__name__)
-cors = CORS(app)
-app.config['CORS_HEADERS'] = 'Content-Type'
+CORS(app)
 
 # Folder path where the images are located
 IMAGE_FOLDER = r'data\body'
@@ -13,16 +12,14 @@ IMAGE_FOLDER = r'data\body'
 
 
 @app.route('/images/<path:filename>')
-@cross_origin()
 def get_image(filename):
     return send_from_directory(IMAGE_FOLDER, filename)
 
-# API route to get image URLs
+# API route to get image URLs and slider heading
 
 
-@app.route('/api/images')
-@cross_origin()
-def get_image_urls():
+@app.route('/api/slider')
+def get_slider_data():
     # Get all image filenames from the folder
     image_files = os.listdir(IMAGE_FOLDER)
     image_urls = []
@@ -31,8 +28,13 @@ def get_image_urls():
     for filename in image_files:
         image_urls.append(f'{IMAGE_FOLDER}\\' + filename)
 
-    return jsonify(image_urls)
+    slider_data = {
+        'heading': 'BODY',
+        'image_urls': image_urls
+    }
+
+    return jsonify(slider_data)
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
